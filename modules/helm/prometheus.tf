e63974a873e8705a49bd3c664e481f55563a6ddf
Justin DeBo <ec2-user@ip-172-31-12-77.ec2.internal>
2024-04-09 01:48:12 +0000

    adding files

--- modules/helm/prometheus.tf
+++ modules/helm/prometheus.tf
@@ -8,7 +8,7 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.wordpress.metadata[0].name
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
   set {
     name  = "service.type"
     value = "LoadBalancer"
